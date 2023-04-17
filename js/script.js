7c297e47e2ad29268ffe949cd90f1b7aab4da86e
enriquecasas <cenriquecasas@gmail.com>
2023-04-17 00:26:35 -0500

    Correcciones de errores al desemcriptar

--- js/script.js
+++ js/script.js
@@ -1,42 +1,66 @@
 var wordencrypt;
-
-function encrypt(){
+var encriptado = false;
+function encrypt() {
     var texto = document.getElementById('texto-area').value.toString();
-    textUser = texto.split("");
-    for (var i = 0; i < textUser.length; i++) {
-        if (textUser[i] == "e") { textUser[i] = "enter" }
-        if (textUser[i] == "i") { textUser[i] = "imes" }
-        if (textUser[i] == "a") { textUser[i] = "ai" }
-        if (textUser[i] == "o") { textUser[i] = "ober" }
-        if (textUser[i] == "u") { textUser[i] = "ufat" }
+
+    if (texto.length == 0 && !encriptado) {
+        document.getElementById("display-imagen").style.display = "";
+        document.getElementById("mensaje-dos").style.display = "";
+        document.getElementById("mensaje-uno").style.display = "";
     }
-
-
-    
-    var textServer = [...textUser].join("");
-    // var p = document.createElement("p");
-    // p.setAttribute("id", "ciphertext");
-    // var ciphertext = document.createTextNode(textServer);
-    // p.appendChild(ciphertext);
-    // document.getElementById("not-display").appendChild(p);
-    // user = true;
-    console.log("ENCRIPTADO", textServer)
-    wordencrypt = textServer;
+    else if ((texto != texto.toLowerCase() || texto != texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "")) && !encriptado) {
+        alert("Se permite sólo letras minúsculas y sin tílde")
+    } else if (!encriptado) {
+        encriptado = true;
+        document.getElementById("display-imagen").style.display = "none";
+        document.getElementById("mensaje-dos").style.display = "none";
+        document.getElementById("mensaje-uno").style.display = "none";
+        document.getElementById("resultado-encontrado").style.display = "";
+        wordencrypt = ""
+        textUser = texto.split("");
+        for (var i = 0; i < textUser.length; i++) {
+            if (textUser[i] == "e") { textUser[i] = "enter" }
+            if (textUser[i] == "i") { textUser[i] = "imes" }
+            if (textUser[i] == "a") { textUser[i] = "ai" }
+            if (textUser[i] == "o") { textUser[i] = "ober" }
+            if (textUser[i] == "u") { textUser[i] = "ufat" }
+        }
+        wordencrypt = [...textUser].join("");
+        document.getElementById("textencript").innerHTML = "";
+        document.getElementById("textencript").innerHTML = wordencrypt;
+    }
 }
 
 
 function decryptor() {
-    var textServer = wordencrypt;
-    if (textServer.includes("enter")) { textServer = textServer.replace(/enter/gi, "e"); }
-    if (textServer.includes("imes")) { textServer = textServer.replace(/imes/gi, "i"); }
-    if (textServer.includes("ai")) { textServer = textServer.replace(/ai/gi, "a"); }
-    if (textServer.includes("ober")) { textServer = textServer.replace(/ober/gi, "o"); }
-    if (textServer.includes("ufat")) { textServer = textServer.replace(/ufat/gi, "u"); }
-    // var p = document.createElement("p");
-    // p.setAttribute("id", "ciphertext");
-    // var ciphertext = document.createTextNode(textServer);
-    // p.appendChild(ciphertext);
-    // document.getElementById("not-display").appendChild(p);
-    // user = true;
-    console.log("DECRIPTADO", textServer)
+    var texto = document.getElementById('texto-area').value.toString();
+
+    if (texto.length > 0 && encriptado) {
+        if (wordencrypt.includes("enter")) { wordencrypt = wordencrypt.replace(/enter/gi, "e"); }
+        if (wordencrypt.includes("imes")) { wordencrypt = wordencrypt.replace(/imes/gi, "i"); }
+        if (wordencrypt.includes("ai")) { wordencrypt = wordencrypt.replace(/ai/gi, "a"); }
+        if (wordencrypt.includes("ober")) { wordencrypt = wordencrypt.replace(/ober/gi, "o"); }
+        if (wordencrypt.includes("ufat")) { wordencrypt = wordencrypt.replace(/ufat/gi, "u"); }
+
+        document.getElementById("textencript").innerHTML = "";
+        document.getElementById("textencript").innerHTML = wordencrypt;
+        encriptado = false;
+    }
+}
+
+
+function limpiar() {
+    document.getElementById("display-imagen").style.display = "";
+    document.getElementById("mensaje-dos").style.display = "";
+    document.getElementById("mensaje-uno").style.display = "";
+    document.getElementById("resultado-encontrado").style.display = "none";
+    document.getElementById("textencript").value = "";
+    document.getElementById("texto-area").value = "";
+    encriptado = false;
+    wordencrypt = "";
+}
+
+function copy() {
+    let textoexriptado = document.getElementById('textencript').value.toString();
+    navigator.clipboard.writeText(textoexriptado.innerHTML);
 }
